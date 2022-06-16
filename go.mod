c38b150ce452587a1838c407b9decd9d43434912
Phil Eagleson <peagleson@symitar.com>
2022-06-16 15:16:01 -0400

    updated symserver agent to properly restarrt and shutdown

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/phileagleson/symserveragentservice
 
 go 1.18
 
-require github.com/kardianos/service v1.2.1
+require (
+	github.com/kardianos/service v1.2.1
+	github.com/phileagleson/symserveragent v0.0.0-00010101000000-000000000000
+)
 
-require golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+require (
+	github.com/phileagleson/ziputils v0.0.0-20220608180218-94096c355153 // indirect
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+)
+
+replace github.com/phileagleson/symserveragent => ../symserveragent
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
 
 	"github.com/kardianos/service"
+	"github.com/phileagleson/symserveragent"
 )
 
 var logger service.Logger
@@ -13,78 +15,109 @@ var logger service.Logger
 type program struct{}
 
 func (p *program) Start(s service.Service) error {
-  // Start should not block. Do the actual work async
-  go p.run()
-  return nil
+	// Start should not block. Do the actual work async
+	go p.run()
+	return nil
 }
 
 func (p *program) run() {
-  logger.Info("started running program...")
+	symserveragent.StartServer()
+	fmt.Println("started running program...")
 }
 
 func (p *program) Stop(s service.Service) error {
-  // Stop should not block. Return with a few seconds.
-  return nil
+	// Stop should not block. Return with a few seconds.
+	err := symserveragent.StopServer()
+	if err != nil {
+		log.Printf("Error stopping sym server agent: %+v\n", err)
+	}
+	return nil
+}
+
+func (p *program) Restart(s service.Service) error {
+	err := p.Stop(s)
+	if err != nil {
+		return err
+	}
+	err = p.Start(s)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
-  svcConfig := &service.Config{
-    Name: "GoServiceExample",
-    DisplayName: "Go Service Example",
-    Description: "This is an example Go service.",
-  }
+	svcConfig := &service.Config{
+		Name:        "SymServerAgent",
+		DisplayName: "Sym Server Agent",
+		Description: "The Sym Server Agent handles file uploads for PowerFrame",
+	}
 
-  prg := &program{}
-  s, err := service.New(prg, svcConfig)
-  if err != nil {
-    log.Fatal(err)
-  }
-  logger, err = s.Logger(nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  // get command line args to specify install, uninstall, start, stop, restart
-  args := os.Args
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  if len(args) > 2 {
-    log.Fatal("Please specify only one argument: install, uninstall, start, stop, or restart")
-  } else if len(args) == 1 {
-    log.Fatal("Please specify a command: install, uninstall, start, stop, or restart")
-  } 
+	// get command line args to specify install, uninstall, start, stop, restart
+	args := os.Args
 
-  command := args[1]
+	var command string
+	if len(args) > 1 {
+		command = args[1]
+	}
 
+	switch strings.ToLower(command) {
+	case "--install":
+		//  log.Println("Installing...")
+		fmt.Println("Installing...")
+		log.Printf("Installing service...\n")
+		err = s.Install()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	case "--uninstall":
+		// log.Println("Uninstalling...")
+		fmt.Println("Uninstalling...")
+		log.Printf("Uninstalling service...\n")
+		err = s.Uninstall()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	case "--start":
+		//log.Println("Starting service...")
+		fmt.Println("Starting service...")
+		log.Printf("Starting service...\n")
+		err = s.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "--stop":
+		//log.Println("Stopping service...")
+		fmt.Println("Stopping service...")
+		log.Printf("Stopping service...\n")
+		err = s.Stop()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	case "--restart":
+		//log.Println("Restarting service...")
+		fmt.Println("Restarting service...")
+		log.Printf("Restarting service...\n")
+		err = s.Restart()
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		s.Run()
 
-  switch strings.ToLower(command) {
-case "--install":
-  log.Println("Installing...")
-  err = s.Install()
-    if err != nil {
-      log.Fatal(err)
-   }
-  return
-case "--uninstall":
-  log.Println("Uninstalling...")
-  s.Uninstall()
-  return
-case "--start":
-  log.Println("Starting service...")
-  s.Run()
-case "--stop":
-  log.Println("Stopping service...")
-  s.Stop()
-  return
-case "--restart":
-  log.Println("Restarting service...")
-  s.Restart()
-  default:
-    log.Fatal("Please specify install, uninstall, start, stop, or restart")
+	}
 
-}
-
-  err = s.Run()
-  if err != nil {
-    logger.Error(err)
-  }
-
+	err = s.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
